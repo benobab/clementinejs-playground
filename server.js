0269acf0229e022356df73ba0147a68aa50b7129
Benobab <benlacroix83@gmail.com>
2016-08-01 15:57:32 +0000

    [SETUP - Add morgan to log err requests only]

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ var routes = require('./app/routes/index.js');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var session = require('express-session');
+var morgan = require('morgan');
 
 var app = express();
 require('dotenv').load();
 require('./app/config/passport')(passport);
 
 mongoose.connect(process.env.MONGO_URI);
+var logger = morgan('combined', {
+  skip: function (req, res) { return res.statusCode < 400 }
+});
 
+app.use(logger);
 app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/common', express.static(process.cwd() + '/app/common'));
